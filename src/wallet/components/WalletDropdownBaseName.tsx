11412b39f0433cf1b1be286e1c5793501d004bde
Paul Cramer <paulccramer@gmail.com>
2024-07-26 18:35:47 -0700

    Cleanerrrrr

--- src/wallet/components/WalletDropdownBaseName.tsx
+++ src/wallet/components/WalletDropdownBaseName.tsx
@@ -1,7 +1,9 @@
+import React from "react";
 import { baseNameSvg } from "../../internal/svg/baseNameSvg";
 import { cn, pressable, text } from "../../styles/theme";
 import { useName } from "../../identity/hooks/useName";
 import { base } from "viem/chains";
+import { Spinner } from '../../internal/components/Spinner';
 
 type WalletDropdownBaseNameReact = {
   className?: string;
@@ -10,27 +12,17 @@ type WalletDropdownBaseNameReact = {
 export function WalletDropdownBaseName({
   className,
 }: WalletDropdownBaseNameReact) {
-  // BUG: Currently returning the ENS name when the base name is being displayed
   const { data: baseName, isLoading } = useName({
     address: "0x4bEf0221d6F7Dd0C969fe46a4e9b339a84F52FDF",
     chain: base,
   });
 
-  let hasBaseUserName = false;
-  if (baseName) {
-    hasBaseUserName = true;
-  }
+  const hasBaseUserName = !!baseName;
 
-  let title: string;
-  let href: string;
-
-  if (hasBaseUserName) {
-    title = "Go to profile";
-    href = `https://www.base.org/name/${baseName}`;
-  } else {
-    title = "Claim a Basename";
-    href = "https://www.base.org/names";
-  }
+  let title = hasBaseUserName ? "Go to profile" : "Claim a Basename";
+  let href = hasBaseUserName
+    ? `https://www.base.org/name/${baseName}`
+    : "https://www.base.org/names";
 
   return (
     <a
@@ -45,13 +37,21 @@ export function WalletDropdownBaseName({
     >
       {baseNameSvg && <div className="h-5 w-5 mr-2">{baseNameSvg}</div>}
       <div className="flex items-center">
-        <span className={cn(text.body, "shrink-0")}>{title}</span>
-        {!hasBaseUserName && (
-          <span className="ml-2 text-[#4F46E5] text-center font-inter text-[0.6875rem] font-bold leading-6 uppercase bg-[#E0E7FF] rounded-full px-2">
-            NEW
-          </span>
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <>
+            <span className={cn(text.body, "shrink-0")}>{title}</span>
+            {!hasBaseUserName && (
+              <span className="ml-2 text-[#4F46E5] text-center font-inter text-[0.6875rem] font-bold leading-6 uppercase bg-[#E0E7FF] rounded-full px-2">
+                NEW
+              </span>
+            )}
+          </>
         )}
       </div>
     </a>
   );
 }
+
+export default WalletDropdownBaseName;
