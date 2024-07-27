c93788c06a56f7110f9e6e968e1f11e75b4995e9
Paul Cramer <paulccramer@gmail.com>
2024-07-27 13:19:41 -0700

    Remove local

--- src/wallet/components/WalletDropdownBaseName.tsx
+++ src/wallet/components/WalletDropdownBaseName.tsx
@@ -1,9 +1,9 @@
-import { base } from "viem/chains";
-import { useName } from "../../identity/hooks/useName";
-import { Spinner } from "../../internal/components/Spinner";
-import { baseNameSvg } from "../../internal/svg/baseNameSvg";
-import { cn, pressable, text } from "../../styles/theme";
-import { useAccount } from "wagmi";
+import { base } from 'viem/chains';
+import { useAccount } from 'wagmi';
+import { useName } from '../../identity/hooks/useName';
+import { Spinner } from '../../internal/components/Spinner';
+import { baseNameSvg } from '../../internal/svg/baseNameSvg';
+import { cn, pressable, text } from '../../styles/theme';
 
 type WalletDropdownBaseNameReact = {
   className?: string;
@@ -25,17 +25,17 @@ export function WalletDropdownBaseName({
     hasBaseUserName = true;
   }
 
-  const title = hasBaseUserName ? "Go to profile" : "Claim a Basename";
+  const title = hasBaseUserName ? 'Go to profile' : 'Claim a Basename';
   const href = hasBaseUserName
     ? `https://www.base.org/name/${baseName}`
-    : "https://www.base.org/names";
+    : 'https://www.base.org/names';
 
   return (
     <a
       className={cn(
         pressable.default,
-        "flex items-center px-4 py-2",
-        className
+        'flex items-center px-4 py-2',
+        className,
       )}
       href={href}
       target="_blank"
@@ -47,7 +47,7 @@ export function WalletDropdownBaseName({
           <Spinner />
         ) : (
           <>
-            <span className={cn(text.body, "shrink-0")}>{title}</span>
+            <span className={cn(text.body, 'shrink-0')}>{title}</span>
             {!hasBaseUserName && (
               <span className="ml-2 rounded-full bg-[#E0E7FF] px-2 text-center font-bold font-inter text-[#4F46E5] text-[0.6875rem] uppercase leading-6">
                 NEW
